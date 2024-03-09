f2add9f388d2252f7284e94a92bf35dbbf31fb45
Sova Vika <sovavika1106@gmail.com>
2024-03-09 10:52:33 +0200

    main.py

--- project.py
+++ project.py
@@ -1,5 +1,23 @@
-str = input("Введіть рядок - ")
-rev_str = ""
-for i in range(len(str), 0, -1):
-  rev_str += str[i]
-print(rev_str)
+list = []
+val = ""
+sum = 0
+answer = 0
+for i in range(2):
+    ans = int(input("Добав значення - "))
+    list.append(ans)
+while True:
+    ask = input("Хочеш додати значення? - ")
+    if ask.lower() == "т" or ask.lower() == "так":
+        ans = int(input("Добав значення - "))
+        list.append(ans)
+    elif ask.lower() == "н" or ask.lower() == "ні":
+        break
+    else:
+        print("Некоректна відповідь")
+for i in range(len(list) + 1):
+    sum += i
+answer = sum / len(list) + 1
+print("Середнє арифметичне - ", answer)
+
+import second
+second.print("Stepan", 15)
